622ff39372d965fefe90f35b77ff795fc76422f4
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-10-11 15:58:15 -0400

    Update characterController to always face mousePosition
    Closes #6

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -5,17 +5,24 @@ public class MainCharacterController : MonoBehaviour {
 
 	public float moveSpeed = 4f;
 	private Vector3 moveDirection;
+	private Vector3 faceDirection;
 	private CharacterController controller;
+	private float offset;  
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController>();
+		offset = Camera.main.transform.position.y - transform.position.y;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		faceDirection = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, offset));
+		transform.LookAt (new Vector3(faceDirection.x, transform.position.y, faceDirection.z));
+
 		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-		Debug.Log (moveDirection);
 		moveDirection = moveDirection.normalized * moveSpeed;
 		controller.Move (moveDirection * Time.deltaTime);
 	}
