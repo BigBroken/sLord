ec7dc1831c95c062f97aac3cea6e794155d8baef
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-10-16 10:19:07 -0400

    Add soil and plant script

---
+++ Assets/Scripts/Soil and Plants/SoilCellController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoilCellController : MonoBehaviour {
+
+
+	//public dictionary plant type
+	void Start () {
+		EventManager.StartListening ("Sleep", NextDay);
+	}
+	void OnDisable(){
+		EventManager.StopListening ("Sleep", NextDay);
+	}
+	void OnDestroy(){
+		EventManager.StopListening ("Sleep", NextDay);
+	}
+
+	void NextDay () {
+	
+	}
+
+	void Sow (int index) {
+		Debug.Log(SoilGridController.plants [index]);
+	}
+}
---
+++ Assets/Scripts/Soil and Plants/SoilGridController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoilGridController : MonoBehaviour {
+
+	public static GameObject[] plants;
+	//I used this to keep track of the number of objects I spawned in the scene.
+	public static int numSpawned = 0;
+
+	void Start()
+	{
+		//Important note: place your prefabs folder(or levels or whatever) 
+		//in a folder called "Resources" like this "Assets/Resources/Prefabs"
+		plants = Resources.LoadAll<GameObject>("Prefabs/Plants");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+}
